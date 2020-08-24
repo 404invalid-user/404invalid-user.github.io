a13f9bcd0caa62bc366322bae8c1454cc3300b65
invalid-user <bruv@invaliduser.uk.to>
2020-08-24 23:01:15 +0100

    Create cool.js

---
+++ assets/cool.js
@@ -0,0 +1,21 @@
+function moreInfo() {
+
+    var x = document.getElementById("aboutText");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
+
+function projects() {
+
+    var x = document.getElementById("projectsText");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+}
